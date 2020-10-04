7af047d9ba5e08d885c06b958739402eeef196d8
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-04 22:21:43 +0700

    Add files via upload

---
+++ precache-manifest.d56f1a2d2bded455fb37566645022c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c30e1a0f860cd514a923ac8bd19e698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94386186fcc2a43ed106",
+    "url": "/static/css/main.bba0a574.chunk.css"
+  },
+  {
+    "revision": "0ad2d4c29cef3b4f3859",
+    "url": "/static/js/2.93dd4f91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.93dd4f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94386186fcc2a43ed106",
+    "url": "/static/js/main.19bbf662.chunk.js"
+  },
+  {
+    "revision": "66ecc91bef8c9109f1dd",
+    "url": "/static/js/runtime-main.ba70cec8.js"
+  }
+]);
